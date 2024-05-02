d7ac3f036bd2a11d6d9d9343d23a5f3ba5a224b2
vitordarcidasilva <vitodarcidasilva@gmail.com>
2024-05-02 00:53:26 -0300

    my project kafka

--- artist/consumer.py
+++ artist/consumer.py
@@ -1,25 +1,44 @@
 from kafka import KafkaConsumer
 import pandas as pd
 import json
+import random
+import string
+import boto3
+from io import StringIO
+
+
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(length))
 
 consumer = KafkaConsumer(
     'artist',
-    bootstrap_servers='pet-eft-11457-us1-kafka.upstash.io:9092',
+    bootstrap_servers='mutual-shrimp-13505-us1-kafka.upstash.io:9092',
     sasl_mechanism='SCRAM-SHA-256',
     security_protocol='SASL_SSL',
-    sasl_plain_username='cGV0LWVmdC0xMTQ1NyQyyYyQflKfRj9QfdWQoY2oXfVvzulQufnkNO9Nv_xHiFY',
-    sasl_plain_password='YzAzZWI3M2UtYmIyYy00YzFlLWI3ZjEtMDZjOWVhMzJjMmI5',
+    sasl_plain_username='bXV0dWFsLXNocmltcC0xMzUwNSTolZoSwUXeZqoOfvlp4xj3pjl4uRXoCbeBmXI',
+    sasl_plain_password='YmY4ZWZjZmItNmE2Yi00Y2ZhLWI1N2ItNTgxMjU2ZGM1NjRm',
     group_id='YOUR_CONSUMER_GROUP',
     auto_offset_reset='earliest'
 )
+# Initialize S3 client
+s3_client = boto3.client('s3')
+
+# Specify the S3 bucket and object key
+bucket_name = 'de-project-kafka'
 
 DATA = []
 try:
     for message in consumer:
         DATA.append(json.loads(message.value))
-        if len(DATA)>=1:
+        if len(DATA)>=10:
             df = pd.DataFrame(DATA)
-            df.to_csv('data.csv',index=False)
+            # df.to_csv('data.csv',index=False)
+            csv_buffer = StringIO()
+            df.to_csv(csv_buffer, index=False)
+            s3_key = "staging/artist/"+generate_random_string(20)+".csv"
+            s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=csv_buffer.getvalue())
+            print(f"DataFrame uploaded successfully to S3 bucket: {bucket_name} with key: {s3_key}")    
             DATA = []
 except KeyboardInterrupt:
     pass
--- artist/producer.py
+++ artist/producer.py
@@ -3,19 +3,16 @@ from kafka import KafkaProducer
 import pandas as pd
 import json
 
-
-
 producer = KafkaProducer(
-    bootstrap_servers='pet-eft-11457-us1-kafka.upstash.io:9092',
+    bootstrap_servers='mutual-shrimp-13505-us1-kafka.upstash.io:9092',
     sasl_mechanism='SCRAM-SHA-256',
     security_protocol='SASL_SSL',
-    sasl_plain_username='cGV0LWVmdC0xMTQ1NyQyyYyQflKfRj9QfdWQoY2oXfVvzulQufnkNO9Nv_xHiFY',
-    sasl_plain_password='YzAzZWI3M2UtYmIyYy00YzFlLWI3ZjEtMDZjOWVhMzJjMmI5',
+    sasl_plain_username='bXV0dWFsLXNocmltcC0xMzUwNSTolZoSwUXeZqoOfvlp4xj3pjl4uRXoCbeBmXI',
+    sasl_plain_password='YmY4ZWZjZmItNmE2Yi00Y2ZhLWI1N2ItNTgxMjU2ZGM1NjRm',
     api_version_auto_timeout_ms=100000,    
 )
 
-
-tracks = pd.read_csv('albums.csv')
+tracks = pd.read_csv('atists.csv')
 
 for dt in tracks.to_dict(orient='records'):
     data = json.dumps(dt).encode('utf-8')
@@ -25,7 +22,4 @@ for dt in tracks.to_dict(orient='records'):
         print("Message produced:", result)
     except Exception as e:
         print(f"Error producing message: {e}")
-    finally:
-        producer.close()
-    
-    break
+producer.close()
